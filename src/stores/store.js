18dd8fe83b7596658a8adc0e3461d4d446611539
Devchiii <devchiii77@gmail.com>
2025-03-04 20:05:48 +1300

    Remove src/store from the repository

--- src/stores/store.js
+++
@@ -1,9 +0,0 @@
-// src/stores.js
-import { writable } from 'svelte/store';
-
-// Creating a writable store for the user selections
-export const userData = writable({
-  mood: '',
-  country: '',
-  weather: ''
-});
