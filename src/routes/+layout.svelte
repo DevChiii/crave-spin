d0d97d66bd067199afc62034c87c1347916f53c7
Devchiii <devchiii77@gmail.com>
2025-03-10 10:36:38 +1300

    Add DCMA verification

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,16 +4,19 @@
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte'; // Import the Footer component
-  
+
 	// Inject analytics tracking
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
-	
+
 	let { children } = $props();
-  </script>
-  
-  <Navbar />
-  <main class="bg-[#F5F5DC] flex-grow"> <!-- Set Light Beige background for the whole page -->
+</script>
+
+<svelte:head>
+	<meta name="dmca-site-verification" content="VlEwWmQ3WlVSSDd6OWQ3emR1R0JsZWUwVUtZZUx3Y3RhVlhRdmZmQmwvMD01" />
+</svelte:head>
+
+<Navbar />
+<main class="bg-[#F5F5DC] flex-grow"> <!-- Set Light Beige background for the whole page -->
 	{@render children()}
-  </main>
-  <Footer /> <!-- Footer component will now blend with the page -->
-  
+</main>
+<Footer /> <!-- Footer component will now blend with the page -->
